8f063a99645b91c6dd1501c4b8a779f5d8841c51
Peter Liang <jlia6158@uni.sydney.edu.au>
2020-02-22 23:50:12 +1100

    Added new data set - Alice Rhee

--- faces-train.py
+++ faces-train.py
@@ -21,19 +21,19 @@ for root, dirs, files in os.walk(image_dir):
 			path = os.path.join(root, file)
 
 			label = os.path.basename(root).replace(" ", "-").lower()
-			# print(label, path)
+			print(label, path)
 			if not label in label_ids:
 				label_ids[label] = current_id
 				current_id += 1
 			id_ = label_ids[label]
-			#print(label_ids)
+			print(label_ids)
 			#y_labels.append(label) # some number
 			#x_train.append(path) # verify this image, turn into a NUMPY arrray, GRAY
 			pil_image = Image.open(path).convert("L") # grayscale
 			size = (550, 550)
 			final_image = pil_image.resize(size, Image.ANTIALIAS)
 			image_array = np.array(final_image, "uint8")
-			# print(image_a rray)
+			print(image_array)
 			faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
 
 			for (x,y,w,h) in faces:
--- faces.py
+++ faces.py
@@ -30,7 +30,7 @@ while(True):
     	# recognize? deep learned model predict keras tensorflow pytorch scikit learn
     	id_, conf = recognizer.predict(roi_gray)
     	if conf>=4 and conf <= 85:
-    		#print(5: #id_)
+    		print(id_)
     		#print(labels[id_])
     		font = cv2.FONT_HERSHEY_SIMPLEX
     		name = labels[id_]
