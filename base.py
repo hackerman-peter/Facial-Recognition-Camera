7dfa8f392dd69edf452b27119f4884c479ba0f13
Peter Liang <jlia6158@uni.sydney.edu.au>
2020-02-22 22:59:05 +1100

    Uploaded Semi-complete facial recognition

--- base.py
+++ base.py
@@ -7,12 +7,8 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     # Display the resulting frame
     cv2.imshow('frame',frame)
-    cv2.imshow('gray',gray)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
